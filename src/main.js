3c287fdd6f4ad3e013d88e4641d52fb1fc5d6fec
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-08 17:13:13 +0330

    Build BaseCard component globaly in app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+
+app.mount('#app');
