891568c9188543c592b0ef68b5ec7cf347ef15f8
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-08 13:58:12 +0330

    Seu up & first steps

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official vue.js documentation',
+          link: 'https://vue.js.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
