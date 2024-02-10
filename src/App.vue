a6008e1ebb0947b652a1cfd6324c308ded5221d5
Mohsen <mohsn.mirzaei@gmail.com>
2024-02-10 11:19:57 +0330

    Implement dynamic components with Attribute Fallthrough

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
   <the-header title="RememberMe" />
-  <stored-resources :resources="storedResources" />
+  <the-resources />
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
-import StoredResources from './components/resources/StoredResources.vue';
+import TheResources from './components/resources/TheResources.vue';
 export default {
   components: {
     TheHeader,
-    StoredResources,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official vue.js documentation',
-          link: 'https://vue.js.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.org',
-        },
-      ],
-    };
+    TheResources,
   },
 };
 </script>
